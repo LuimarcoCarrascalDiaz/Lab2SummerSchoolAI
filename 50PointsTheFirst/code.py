c167427a1b8b3eb20e13d4e093be9a250eb355f4
LuimarcoCarrascalDiaz <166428428+LuimarcoCarrascalDiaz@users.noreply.github.com>
2024-07-19 01:20:56 -0500

    Update code.py

--- 50PointsTheFirst/code.py
+++ 50PointsTheFirst/code.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 
 
-genai.configure(api_key="AIzaSyADAzhsx3WDJmLwg5KPi_joaDzyBLvIS3E")
+genai.configure(api_key="API_KEY")
 model = genai.GenerativeModel('gemini-pro')
 
 prompt = "Write a story about a magic backpack that can grant wishes."
