36a817065f9263ef564088843bbade2d04ffd534
LuimarcoCarrascalDiaz <166428428+LuimarcoCarrascalDiaz@users.noreply.github.com>
2024-07-19 03:53:28 -0500

    Update codeSample3.py

--- 50PointsTheFirst/codeSample3.py
+++ 50PointsTheFirst/codeSample3.py
@@ -1,1 +1,9 @@
+import google.generativeai as genai
 
+
+genai.configure(api_key="API_KEY")
+model = genai.GenerativeModel('gemini-pro')
+
+prompt = "you are a systems and computing engineering"
+response = model.generate_content(prompt)
+print(response.text)
