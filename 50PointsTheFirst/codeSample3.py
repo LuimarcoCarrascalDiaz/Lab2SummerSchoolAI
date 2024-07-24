738f679b8327197a631cc47e337086ac5589fdd0
LuimarcoCarrascalDiaz <166428428+LuimarcoCarrascalDiaz@users.noreply.github.com>
2024-07-24 00:08:50 -0500

    Update codeSample3.py

--- 50PointsTheFirst/codeSample3.py
+++ 50PointsTheFirst/codeSample3.py
@@ -4,6 +4,6 @@ import google.generativeai as genai
 genai.configure(api_key="API_KEY")
 model = genai.GenerativeModel('gemini-pro')
 
-prompt = "you are a systems and computing engineering"
+prompt = "Write a concise but engaging blogpost about DeepLearning.AI. Make sure the blogpost iswithin 100 words."
 response = model.generate_content(prompt)
 print(response.text)
