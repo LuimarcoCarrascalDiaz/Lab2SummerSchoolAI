23e1d1a85a52cf7044569fc42dab164c66db5286
LuimarcoCarrascalDiaz <166428428+LuimarcoCarrascalDiaz@users.noreply.github.com>
2024-07-24 00:14:00 -0500

    Update codesample4.py

--- 50PointsTheFirst/codesample4.py
+++ 50PointsTheFirst/codesample4.py
@@ -1,1 +1,9 @@
+import google.generativeai as genai
 
+
+genai.configure(api_key="API_KEY")
+model = genai.GenerativeModel('gemini-pro')
+
+prompt = "You are an SEO reviewer"
+response = model.generate_content(prompt)
+print(response.text)
