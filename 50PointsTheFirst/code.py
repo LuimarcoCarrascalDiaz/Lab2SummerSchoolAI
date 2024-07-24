2d6bafcbc70d4b5819b8d29173d8f17e6cd0af28
LuimarcoCarrascalDiaz <166428428+LuimarcoCarrascalDiaz@users.noreply.github.com>
2024-07-23 23:58:03 -0500

    Update code.py

--- 50PointsTheFirst/code.py
+++ 50PointsTheFirst/code.py
@@ -4,6 +4,6 @@ import google.generativeai as genai
 genai.configure(api_key="API_KEY")
 model = genai.GenerativeModel('gemini-pro')
 
-prompt = "Write a story about a magic backpack that can grant wishes."
+prompt = "Your name is Cathy and you are a stand-up comedian."
 response = model.generate_content(prompt)
 print(response.text)
