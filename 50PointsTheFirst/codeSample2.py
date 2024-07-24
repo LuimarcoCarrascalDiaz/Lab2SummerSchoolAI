d67a87aad88e99cbd93b3b56e25663b7a0ac5bae
LuimarcoCarrascalDiaz <166428428+LuimarcoCarrascalDiaz@users.noreply.github.com>
2024-07-24 00:06:21 -0500

    Update codeSample2.py

--- 50PointsTheFirst/codeSample2.py
+++ 50PointsTheFirst/codeSample2.py
@@ -4,6 +4,6 @@ import google.generativeai as genai
 genai.configure(api_key="API_KEY")
 model = genai.GenerativeModel('gemini-pro')
 
-prompt = "you are a lawyer consultant"
+prompt = "You are a helpful customer service agenthere to provide fun for the customer based on the user'spersonal information and topic preferences.This could include fun facts, jokes, or interesting stories. Make sure to make it engaging and fun!Return 'TERMINATE' when you are done"
 response = model.generate_content(prompt)
 print(response.text)
